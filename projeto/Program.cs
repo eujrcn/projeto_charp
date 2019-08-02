373152a83becf08f20cbc1d2fb3a15edc928dfdc
Roberto <eu.jrcn@gmail.com>
2019-08-02 14:48:47 -0300

    Inicio vamo que vamo

---
+++ projeto/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace projeto {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Vamo que Vamo");
+            Console.WriteLine("legal");
+        }
+    }
+}
